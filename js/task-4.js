926f03cac7845bbd84d41a7b1996bfc345d701bd
Roman <roman.anokhin.work@gmail.com>
2025-08-22 17:06:17 +0300

    Update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -5,12 +5,17 @@ form.addEventListener("submit", handleSubmit);
 function handleSubmit(event) {
   event.preventDefault();
   const elements = event.target.elements;
-  if (elements.email.value === "" || elements.password.value === "") {
+  const emailName = elements.email.name.trim();
+  const passwordName = elements.password.name.trim();
+  const emailValue = elements.email.value.trim();
+  const passwordValue = elements.password.value.trim();
+
+  if (emailValue === "" || passwordValue === "") {
     alert("All form fields must be filled in");
   } else {
     const obj = {
-      [elements.email.name]: elements.email.value.trim(),
-      [elements.password.name]: elements.password.value.trim(),
+      [emailName]: emailValue,
+      [passwordName]: passwordValue,
     };
 
     console.log(obj);
